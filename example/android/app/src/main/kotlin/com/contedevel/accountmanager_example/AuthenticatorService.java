69d7f866ac3a8a2bedb8668fa4197f3aece325e4
Denis Sologub <contedevel2010@gmail.com>
2020-05-17 02:57:12 +0300

    Add authenticator

---
+++ example/android/app/src/main/kotlin/com/contedevel/accountmanager_example/AuthenticatorService.java
@@ -0,0 +1,16 @@
+package com.contedevel.accountmanager_example;
+
+import android.app.Service;
+import android.content.Intent;
+import android.os.IBinder;
+
+import androidx.annotation.Nullable;
+
+public class AuthenticatorService extends Service {
+    @Nullable
+    @Override
+    public IBinder onBind(Intent intent) {
+        Authenticator authenticator = new Authenticator(this);
+        return authenticator.getIBinder();
+    }
+}
