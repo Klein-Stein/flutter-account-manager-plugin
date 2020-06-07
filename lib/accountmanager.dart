5c37d909f1c280857f1f80d353b2d3b906b5a53d
Denis Sologub <contedevel2010@gmail.com>
2020-06-07 09:15:14 +0300

    Upgrade Gradle

--- lib/accountmanager.dart
+++ lib/accountmanager.dart
@@ -25,17 +25,17 @@ class AccountManager {
   }
 
   static Future<List<dynamic>> getAccounts() async {
+    var accounts = [];
     try {
       final result = await _channel.invokeListMethod('getAccounts');
-      var accounts = [];
       for (var item in result) {
         accounts.add(new Account(item[_KeyAccountName], item[_KeyAccountType]));
       }
-      return accounts;
     } catch(e, s) {
       print(e);
       print(s);
     }
+    return accounts;
   }
 
   static Future removeAccount(Account account) async {
