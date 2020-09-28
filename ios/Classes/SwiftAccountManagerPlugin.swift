8dcd4ffd142624d86027fc33fe97a6b6b0796320
Denis Sologub <contedevel2010@gmail.com>
2020-09-28 03:32:50 +0300

    Add iOS support, upgrade third-party libs

--- ios/Classes/SwiftAccountManagerPlugin.swift
+++ ios/Classes/SwiftAccountManagerPlugin.swift
@@ -2,9 +2,12 @@ import Flutter
 import UIKit
 
 public class SwiftAccountManagerPlugin: NSObject, FlutterPlugin {
+    public let constAccountsPreference = "com.contedevel.account_manager.accounts";
     public let constAccountName = "account_name";
     public let constAccountType = "account_type";
-    public let constPassword = "password";
+    public let constAuthTokenType = "auth_token_type";
+    public let constAccessToken = "access_token";
+    public let constRefreshToken = "refresh_token";
     
     public static func register(with registrar: FlutterPluginRegistrar) {
         let channel = FlutterMethodChannel(name: "accountManager", binaryMessenger: registrar.messenger())
@@ -13,19 +16,51 @@ public class SwiftAccountManagerPlugin: NSObject, FlutterPlugin {
     }
     
     private func addAccount(_ call: FlutterMethodCall, result: @escaping FlutterResult) {
-        result(true);
+        if let args = call.arguments as? [String: String] {
+            let preferences = UserDefaults.standard;
+            var accounts = preferences.array(forKey: constAccountsPreference) as? [[String: String?]] ?? [[String: String?]]();
+            if !accounts.contains(where: { $0[constAccountType] == args[constAccountType] &&
+                                    $0[constAccountName] == args[constAccountName] }) {
+                accounts.append([
+                    constAccountName: args[constAccountName],
+                    constAccountType: args[constAccountType],
+                    constAuthTokenType: args[constAuthTokenType],
+                    constAccessToken: args[constAccessToken],
+                    constRefreshToken: args[constRefreshToken]
+                ]);
+                preferences.set(accounts, forKey: constAccountsPreference);
+                result(true);
+                return;
+            }
+        }
+        result(false);
     }
     
     private func removeAccount(_ call: FlutterMethodCall, result: @escaping FlutterResult) {
-        result(true);
+        if let args = call.arguments as? [String: String] {
+            let preferences = UserDefaults.standard;
+            var accounts = preferences.array(forKey: constAccountsPreference) as? [[String: String?]] ?? [[String: String?]]();
+            accounts = accounts.filter { $0[constAccountType] != args[constAccountType] || $0[constAccountName] != args[constAccountName] };
+            preferences.set(accounts, forKey: constAccountsPreference);
+            result(true);
+        } else {
+            result(false);
+        }
     }
     
     private func getAccounts(_ call: FlutterMethodCall, result: @escaping FlutterResult) {
-        var accounts: [[String: Any]] = [];
-        accounts.append([
-            constAccountName: "contedevel2010@gmail.com",
-            constAccountType: "com.contedevel.account"
-        ]);
+        var data: [[String: String?]] = [];
+        let preferences = UserDefaults.standard;
+        let accounts = preferences.array(forKey: constAccountsPreference) as? [[String:String]] ?? [[String: String]]();
+        for account in accounts {
+            data.append([
+                constAccountName: account[constAccountName],
+                constAccountType: account[constAccountType],
+                constAuthTokenType: account[constAuthTokenType],
+                constAccessToken: account[constAccessToken],
+                constRefreshToken: account[constRefreshToken]
+            ])
+        }
         result(accounts);
     }
     
--- lib/accountmanager.dart
+++ lib/accountmanager.dart
@@ -5,8 +5,13 @@ import 'package:flutter/services.dart';
 class Account {
   String name;
   String accountType;
+  String authTokenType;
+  String accessToken;
+  String refreshToken;
 
-  Account(this.name, this.accountType);
+
+  Account(this.name, this.accountType, this.authTokenType,
+          this.accessToken, this.refreshToken);
 }
 
 class AccountManager {
@@ -14,14 +19,18 @@ class AccountManager {
 
   static const String _KeyAccountName = 'account_name';
   static const String _KeyAccountType = 'account_type';
-  static const String _KeyPassword = 'password';
+  static const String _KeyAuthTokenType = 'auth_token_type';
+  static const String _KeyAccessToken = 'access_token';
+  static const String _KeyRefreshToken = 'refreshToken';
 
-  static Future<void> addAccount(Account account, {String password}) async {
+  static Future<void> addAccount(Account account) async {
     try {
       await _channel.invokeMethod('addAccount', {
         _KeyAccountName: account.name,
         _KeyAccountType: account.accountType,
-        _KeyPassword: password
+        _KeyAuthTokenType: account.authTokenType,
+        _KeyAccessToken: account.accessToken,
+        _KeyRefreshToken: account.refreshToken
       });
     } catch(e, s) {
       print(e);
@@ -34,7 +43,9 @@ class AccountManager {
     try {
       final result = await _channel.invokeMethod('getAccounts');
       for (var item in result) {
-        accounts.add(new Account(item[_KeyAccountName], item[_KeyAccountType]));
+        accounts.add(new Account(
+            item[_KeyAccountName], item[_KeyAccountType], null, null, null
+        ));
       }
     } catch(e, s) {
       print(e);
